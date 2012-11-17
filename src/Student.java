afa8dcde06111ed17d03d48904896eb15895b5c0
Jkettler <jimmy@schooladmin.com>
2012-11-16 23:44:40 -0600

    fuckalgo

--- src/Student.java
+++ src/Student.java
@@ -6,44 +6,46 @@ public class Student {
 	public int classSize;
 	public int numTiers;
 	public int maxTierSize = 0;
+	public ArrayList<Integer> outs = new ArrayList<Integer>();
+	public int hopefulTier;
 
 
 	public Student(String s, int index){
 		id = index;
+		hopefulTier = 0;
 		String[] tempStringTierList = s.trim().split("\\s-1\\s");
 		numTiers = tempStringTierList.length;
 		for(int t = 0; t < numTiers; t++){
 			String[] temp = tempStringTierList[t].trim().split("[\\s]");
-			//System.out.println("temp has leng: " + temp.length);
 			if(temp.length > maxTierSize) maxTierSize = temp.length;
 
 			ArrayList<Integer> temp2 = new ArrayList<Integer>();
 			for(int j = 0; j < temp.length; j++){
-				
+
 				temp2.add(Integer.parseInt(temp[j]));
 			}
 			tierList.add((ArrayList<Integer>) temp2);
-			tierList.trimToSize();
 		}
-		//System.out.println("maxTierSize is : " + maxTierSize);
+		tierList.trimToSize();
+		outs = tierList.get(hopefulTier);
 	}
 
 	public void print2Darrayints(){
-	
+
 		for(ArrayList<Integer> i : tierList){
 			System.out.print("[");
-			
+
 			for(int j : i){	
 				System.out.print( "[" + j + "]");
 			}
 			System.out.print("]");
 			System.out.println();
 		}
-		
+
 	}
-	
-//	public String print1Darraystrings(){
-//		for(int i = 0; i < )
-//	}
+
+	//	public String print1Darraystrings(){
+	//		for(int i = 0; i < )
+	//	}
 
 }
